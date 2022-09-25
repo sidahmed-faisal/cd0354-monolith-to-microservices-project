39b66011de5c2bc76d91195381e64e978e5118e8
Sidahmed <83870963+sidahmed-faisal@users.noreply.github.com>
2022-09-25 20:32:39 +0200

    RDS new session

--- set_env.sh
+++ set_env.sh
@@ -2,7 +2,7 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=postgres
-export POSTGRES_HOST=postgres.cyc9xz8tsp9w.us-west-1.rds.amazonaws.com
+export POSTGRES_HOST=postgres.c7diagpvcf3u.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_MEDIA_BUCKET=workbucklol
 export AWS_REGION=us-west-1
