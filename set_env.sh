7d28645901d265fcb83f747cb69b7078527b0176
Sidahmed <83870963+sidahmed-faisal@users.noreply.github.com>
2022-09-30 20:31:53 +0200

    test feed with new credentials

--- set_env.sh
+++ set_env.sh
@@ -2,10 +2,10 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=postgres
-export POSTGRES_HOST=postgres.c7diagpvcf3u.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=postgres.cql1aknrfsdr.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_MEDIA_BUCKET=workbucklol
-export AWS_REGION=us-west-1
+export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=hello
 export URL=http://localhost:8100
